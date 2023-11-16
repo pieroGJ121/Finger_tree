159e67bfd42ba5cbde0192df271d3ab7700c337d
pieroGJ121 <piero.guerrero@utec.edu.pe>
2023-11-16 04:02:50 -0500

    finger_tree.h: Add basic form of finger tree

---
+++ finger_tree.h
@@ -0,0 +1,46 @@
+#ifndef FINGER_TREE_H_
+#define FINGER_TREE_H_
+
+#include "node.h"
+#include <stdexcept>
+
+using namespace std;
+
+template <typename T> class Finger_tree {
+  FingerNode<T> *root;
+
+public:
+  Finger_tree() { root = nullptr; }
+  ~Finger_tree() { root->killSelf(); }
+
+  void push_front(T value) {
+    if (root == nullptr) {
+      root = new FingerNode<T>;
+    } else {
+      root->push_front(value);
+    }
+  }
+  void push_back(T value) {
+    if (root == nullptr) {
+      root = new FingerNode<T>;
+    } else {
+      root->push_back(value);
+    }
+  }
+  void pop_front() {
+    if (root == nullptr) {
+      throw std::runtime_error("No elements in finger tree.");
+    } else {
+      root->pop_front();
+    }
+  }
+  void pop_back() {
+    if (root == nullptr) {
+      throw std::runtime_error("No elements in finger tree.");
+    } else {
+      root->pop_back();
+    }
+  }
+};
+
+#endif // FINGER_TREE_H_
