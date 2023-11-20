ba195471048e4c9634399808d1a5bddd5ecf3012
pieroGJ121 <piero.guerrero@utec.edu.pe>
2023-11-20 12:22:10 -0500

    node.h: Use circular array in place of queue

--- node.h
+++ node.h
@@ -1,14 +1,14 @@
 #ifndef NODE_H_
 #define NODE_H_
 
-#include "queue.h"
+#include "circulararray.h"
 #include <exception>
 #include <queue>
 
 using namespace std;
 
 template <typename T> class Node {
-  Queue<Node<T> *> *children;
+  CircularArray<Node<T> *> *children;
   T value;
   char state;
 
@@ -18,28 +18,32 @@ public:
     state = 'V';
   }
   Node() {
-    children = new Queue<Node<T> *>(3);
+    children = new CircularArray<Node<T> *>(3);
     state = 'N';
   }
   ~Node() { this->killSelf(); }
   void killSelf() { this->children->killSelfPointer(); }
-  Node<T> *pop() { return this->children->dequeue(); }
-  void push(Node<T> *value) { this->children->enqueue(value); }
+  Node<T> *pop_front() { return this->children->pop_front(); }
+  Node<T> *pop_back() { return this->children->pop_back(); }
+  void push_front(Node<T> *value) { this->children->push_front(value); }
+  void push_back(Node<T> *value) { this->children->push_back(value); }
   int size() { return this->children->size(); }
 };
 
 template <typename T> class Affix {
 protected:
-  Queue<Node<T> *> *children;
+  CircularArray<Node<T> *> *children;
 
 public:
-  Affix() { children = new Queue<Node<T> *>(3); }
+  Affix() { children = new CircularArray<Node<T> *>(3); }
   ~Affix() { this->killSelf(); }
   void killSelf() { this->children->killSelfPointer(); }
 
   Node<T> *top() { return this->children->top(); }
-  Node<T> *pop() { return this->children->dequeue(); }
-  void push(Node<T> *value) { this->children->enqueue(value); }
+  Node<T> *pop_front() { return this->children->pop_front(); }
+  Node<T> *pop_back() { return this->children->pop_back(); }
+  void push_front(Node<T> *value) { this->children->push_front(value); }
+  void push_back(Node<T> *value) { this->children->push_back(value); }
   int size() { return this->children->size(); }
 };
 
@@ -53,7 +57,7 @@ public:
   FingerNode() {
     preffix = nullptr;
     suffix = nullptr;
-    next = new FingerNode<T>();
+    next = nullptr;
     state = 'E';
   }
   ~FingerNode() {}
@@ -63,24 +67,25 @@ public:
     // The finger tree doesn't have elements, so it becomes a single
     if (state == 'E') {
       preffix = new Affix<T>();
-      preffix->push(value);
+      preffix->push_front(value);
       this->state = 'S';
+      next = new FingerNode<T>();
       // The finger tree is a single, so it stops being one
     } else if (state == 'S') {
       suffix = new Affix<T>();
-      suffix->push(preffix->pop());
-      preffix->push(value);
+      suffix->push_front(preffix->pop_back());
+      preffix->push_front(value);
       this->state = 'F';
       // The preffix is full
-    } else if (preffix->size == 4) {
+    } else if (preffix->size() == 4) {
       Node<T> *to_add = new Node<T>();
-      to_add->push(preffix->pop());
-      to_add->push(preffix->pop());
-      to_add->push(preffix->pop());
+      to_add->push_front(preffix->pop_back());
+      to_add->push_front(preffix->pop_back());
+      to_add->push_front(preffix->pop_back());
       next->push_front(to_add);
     } else {
       // The preffix has less than 4 elements
-      preffix->push(value);
+      preffix->push_front(value);
     }
   }
 
@@ -89,41 +94,42 @@ public:
     // The finger tree doesn't have elements, so it becomes a single
     if (state == 'E') {
       preffix = new Affix<T>();
-      preffix->push(value);
+      preffix->push_front(value);
+      next = new FingerNode<T>();
       this->state = 'S';
       // The finger tree is a single, so it stops being one
     } else if (state == 'S') {
       suffix = new Affix<T>();
-      suffix->push(value);
+      suffix->push_front(value);
       this->state = 'F';
       // The preffix is full
-    } else if (preffix->size == 4) {
+    } else if (suffix->size() == 4) {
       Node<T> *to_add = new Node<T>();
-      to_add->push(suffix->pop());
-      to_add->push(suffix->pop());
-      to_add->push(suffix->pop());
+      to_add->push_back(suffix->pop_front());
+      to_add->push_back(suffix->pop_front());
+      to_add->push_back(suffix->pop_front());
       next->push_front(to_add);
-      suffix->push(value);
+      suffix->push_back(value);
     } else {
       // The suffix has less than 4 elements
-      suffix->push(value);
+      suffix->push_back(value);
     }
   }
   Node<T> *pop_front() {
     if (state == 'E') {
       return nullptr;
     } else if (state == 'S') {
-      Node<T> *temp = this->preffix->pop();
+      Node<T> *temp = this->preffix->pop_back();
       preffix = nullptr;
       state = 'E';
       return temp;
     } else {
-      Node<T> *temp = preffix->pop();
-      if (preffix->size == 0) {
+      Node<T> *temp = preffix->pop_front();
+      if (preffix->size() == 0) {
         Node<T> *to_add = next->pop_front();
         if (to_add == nullptr) {
           // borrow from suffix
-          preffix->push(suffix->pop());
+          preffix->push_back(suffix->pop_front());
 
           // if only 1 left in suffix, turn finger node into single
           if (suffix->size() == 0) {
@@ -133,7 +139,7 @@ public:
           }
         } else {
           for (int i = 0; i < to_add->size(); i++) {
-            preffix->push(to_add->pop());
+            preffix->push_back(to_add->pop_front());
           }
         }
       }
@@ -144,17 +150,18 @@ public:
     if (state == 'E') {
       return nullptr;
     } else if (state == 'S') {
-      Node<T> *temp = this->preffix->pop();
+      Node<T> *temp = this->preffix->pop_back();
+      this->preffix->killSelf();
       preffix = nullptr;
       state = 'E';
       return temp;
     } else {
-      Node<T> *temp = suffix->pop();
-      if (suffix->size == 0) {
+      Node<T> *temp = suffix->pop_back();
+      if (suffix->size() == 0) {
         Node<T> *to_add = next->pop_back();
         if (to_add == nullptr) {
           // borrow from suffix
-          suffix->push(preffix->pop());
+          suffix->push_front(preffix->pop_back());
           // if only 1 left in preffix, turn finger node into single
           if (preffix->size() == 0) {
             preffix->killSelf();
@@ -163,7 +170,7 @@ public:
           }
         } else {
           for (int i = 0; i < to_add->size(); i++) {
-            suffix->push(to_add->pop());
+            suffix->push_front(to_add->pop_back());
           }
         }
       }
