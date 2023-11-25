77f3049699987181bc3a4bc23c3de6c3ebd21438
pieroGJ121 <piero.guerrero@utec.edu.pe>
2023-11-24 19:25:00 -0500

    test.cpp: Add test for 2-3 tree of level 1

--- test.cpp
+++ test.cpp
@@ -1,9 +1,10 @@
 #include "finger_tree.h"
+#include "node.h"
 #include <iostream>
 
 using namespace std;
 
-int main() {
+void print_finger() {
   Finger_tree<int> *finger = new Finger_tree<int>();
   finger->push_front(2);
   cout << finger->toString(" ") << endl;
@@ -27,5 +28,33 @@ int main() {
   cout << finger->toString(" ") << endl;
   cout << "after push" << endl;
   cout << finger->toString(" ") << endl;
+}
+
+void tree_level1() {
+  sf::RenderWindow window(sf::VideoMode(1200, 1200), "Finger Tree");
+  Node<int> *right = new Node<int>(4);
+  Node<int> *middle = new Node<int>(5);
+  Node<int> *left = new Node<int>(6);
+  Node<int> *level1 = new Node<int>();
+  level1->push_back(right);
+  level1->push_back(middle);
+  level1->push_back(left);
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed)
+        window.close();
+    }
+
+    window.clear(sf::Color::Black);
+
+    draw_node(window, level1, 1, 100, 100);
+
+    window.display();
+  }
+}
+
+int main() {
+  tree_level1();
   return 0;
 }
