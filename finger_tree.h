ea2213f769a04ce6cf81798d0a8381796a9309dc
pieroGJ121 <piero.guerrero@utec.edu.pe>
2023-11-25 03:51:49 -0500

    finger_tree.h: Add draw affix

--- finger_tree.h
+++ finger_tree.h
@@ -87,33 +87,41 @@ void draw_node(sf::RenderWindow &window, Node<T> *node, int level, int pos_x,
 }
 
 template <typename T>
-void draw_affix(sf::RenderWindow &window, Affix<T> *affix, int level,
-                int offsety, bool suffix) {
-  // if the center of the finger tree is in the middle of the screen, the affix
-  // is to the left or to the right. The right indicates whether the it's suffix
-  // when true, otherwise false is for preffix
+void draw_affix(sf::RenderWindow &window, Affix<T> *affix, int level, int pos_x,
+                int pos_y) {
+  sf::CircleShape circle(circle_radius);
+  circle.setPosition(pos_x, pos_y);
+  circle.setFillColor(sf::Color::Blue);
 
-  // First, draw the affix node. Calculate it's x position either dinamically to
-  // have it as close to the finger as possible or harcode it into the middle of
-  // the free space and build the children divding the space evenly in the free
-  // space
+  int x = pos_x;
+  int y = pos_y + circle_radius * 3;
+  int step = pow(3, level + 1) * circle_radius;
 
+  cout << "size: " << affix->size() << endl;
+  cout << "step: " << step << endl;
+
+  // The starting position of x works according to the size of values and level
+  if (affix->size() == 2) {
+    x -= pow(3, level) * circle_radius + circle_radius * 1.5 * level;
+  } else if (affix->size() == 3) {
+    x -= pow(3, level + 1) * circle_radius;
+  } else if (affix->size() == 4) {
+    x -= pow(3, level + 1.3) * circle_radius;
+  }
   // Iterates over each children to draw the children until the end
   for (int i = 0; i < affix->size(); i++) {
-    // Level 1 only has the elements directly in the affix. They are saved as
-    // nodes for ease of use. Level 2 has nodes holding more values
-    for (int i = 0; i < level; i++) {
-      int posx,
-          posy; // The pos x is gonna depend of the level of affix, because
-                // the first level only needs to hold the affix and the
-                // maximun 4 children while the second level can hold up to
-                // 4 nodes and each node can have up to 3 children. Test the
-                // size of the circle to make it adequate. It exists as a
-                // variable now.
-                // pos y is gonna depend on the level and the size of the circle
-                // and lines. Try for the appropiate
-    }
+    cout << "i: " << i << endl;
+    sf::Vertex line[] = {
+        sf::Vertex(sf::Vector2f(pos_x + circle_radius, pos_y + circle_radius),
+                   sf::Color::Black),
+        sf::Vertex(sf::Vector2f(x + circle_radius, y + circle_radius),
+                   sf::Color::Black)};
+    window.draw(line, 2, sf::Lines);
+
+    draw_node(window, (*affix)[i], level, x, y);
+    x += step;
   }
+  window.draw(circle);
 }
 
 template <typename T>
