b8eebea0cf393eb09fbac23f73a0730659c93667
Leonardo HC <107220070+LeonardoHuaman@users.noreply.github.com>
2023-11-26 14:13:06 -0500

    Menu Leyenda

--- main.cpp
+++ main.cpp
@@ -9,86 +9,101 @@
 using namespace std;
 
 int main() {
-  sf::RenderWindow window(sf::VideoMode(1900, 1070), "Matriz SFML");
-  window.setKeyRepeatEnabled(false);
+    sf::RenderWindow window(sf::VideoMode(1900, 1070), "Matriz SFML");
+    window.setKeyRepeatEnabled(false);
 
-  string opcion = "";
-  string message = "";
-  int in_push = 0;
-  string value = "";
+    string opcion = "";
+    string message = "";
+    int in_push = 0;
+    string value = "";
 
-  int ytext = 30;
-  sf::Font font;
-  font.loadFromFile("SulphurPoint-Regular.ttf");
+    int ytext = 30;
+    sf::Font font;
+    font.loadFromFile("SulphurPoint-Regular.ttf");
 
-  Finger_tree<string> *finger = new Finger_tree<string>();
+    Finger_tree<string> *finger = new Finger_tree<string>();
 
-  while (window.isOpen()) {
-    sf::Event event;
+    while (window.isOpen()) {
+        sf::Event event;
 
-    window.clear(sf::Color::White);
+        window.clear(sf::Color::White);
 
-    sf::Text inputText;
-    if (in_push == 0) {
-      inputText = sf::Text(sf::String("1. Push front, "
-                                      "2. Push back, "
-                                      "3. Pop front, "
-                                      "4. Pop back, "
-                                      "5. Cerrar Aplicacion. "
-                                      "Ingrese su opcion: "),
-                           font);
-    } else {
-      inputText =
-          sf::Text(sf::String(opcion + "Ingrese su valor: " + value), font);
-    }
+        sf::Text inputText;
+        if (in_push == 0) {
+            inputText = sf::Text(sf::String("1. Push front, "
+                                            "2. Push back, "
+                                            "3. Pop front, "
+                                            "4. Pop back, "
+                                            "5. Cerrar Aplicacion. "
+                                            "Ingrese su opcion: "),
+                                font);
+        } else {
+            inputText =
+                sf::Text(sf::String(opcion + "Ingrese su valor: " + value), font);
+        }
 
-    inputText.setPosition(20, ytext);
-    inputText.setCharacterSize(24);
-    inputText.setFillColor(sf::Color::Black);
-    window.draw(inputText);
+        inputText.setPosition(20, ytext);
+        inputText.setCharacterSize(24);
+        inputText.setFillColor(sf::Color::Black);
+        window.draw(inputText);
 
-    draw_finger_tree(window, finger, ytext + 45);
+        draw_finger_tree(window, finger, ytext + 45);
 
-    // Display message in the upper right corner
-    sf::Text messageText(message, font);
-    messageText.setCharacterSize(18);
-    messageText.setFillColor(sf::Color::Red);
-    sf::FloatRect textRect = messageText.getLocalBounds();
-    messageText.setPosition(window.getSize().x - textRect.width - 20, 10);
-    window.draw(messageText);
+        // Display message in the upper right corner
+        sf::Text messageText(message, font);
+        messageText.setCharacterSize(18);
+        messageText.setFillColor(sf::Color::Red);
+        sf::FloatRect textRect = messageText.getLocalBounds();
+        messageText.setPosition(window.getSize().x - textRect.width - 20, 10);
+        window.draw(messageText);
 
-    const sf::Color COLOR_PLOMO = sf::Color(128, 128, 128);   // Gris (plomo)
-    const sf::Color COLOR_ROJO = sf::Color::Red;
-    const sf::Color COLOR_AZUL = sf::Color::Blue;
-    const sf::Color COLOR_AMARILLO = sf::Color::Yellow;
+        // Ajusta las posiciones de las leyendas
+        float leyendaY = messageText.getPosition().y + textRect.height + 10;
+        float leyendaX = window.getSize().x - 20;  // Alinea a la derecha
 
-    // Leyendas
-    sf::Text leyendaPLOMO("PLOMO: Finger vacio", font);
-    leyendaPLOMO.setCharacterSize(24);
-    leyendaPLOMO.setFillColor(COLOR_PLOMO);
-    leyendaPLOMO.setPosition(20, window.getSize().y - 20);
+        const sf::Color COLOR_PLOMO = sf::Color(209, 211, 212);
+        const sf::Color COLOR_ROJO = sf::Color::Red;
+        const sf::Color COLOR_AZUL = sf::Color::Blue;
+        const sf::Color COLOR_AMARILLO = sf::Color(209, 203, 10);
+        const sf::Color COLOR_NEGRO = sf::Color::Black;
 
-    sf::Text leyendaROJO("   ROJO: Finger de un solo elemento", font);
-    leyendaROJO.setCharacterSize(24);
-    leyendaROJO.setFillColor(COLOR_ROJO);
-    leyendaROJO.setPosition(leyendaPLOMO.getPosition().x + leyendaPLOMO.getLocalBounds().width + 20, window.getSize().y - 20);
+        sf::Text leyendaPLOMO("PLOMO: Finger vacio", font);
+        leyendaPLOMO.setCharacterSize(24);
+        leyendaPLOMO.setFillColor(COLOR_PLOMO);
 
-    sf::Text leyendaAZUL("   AZUL: Nodo sufijo o prefijo", font);
-    leyendaAZUL.setCharacterSize(24);
-    leyendaAZUL.setFillColor(COLOR_AZUL);
-    leyendaAZUL.setPosition(leyendaROJO.getPosition().x + leyendaROJO.getLocalBounds().width + 20, window.getSize().y - 20);
+        leyendaPLOMO.setPosition(leyendaX - leyendaPLOMO.getLocalBounds().width, leyendaY);
 
-    sf::Text leyendaAMARILLO("   AMARILLO: Nodo", font);
-    leyendaAMARILLO.setCharacterSize(24);
-    leyendaAMARILLO.setFillColor(COLOR_AMARILLO);
-    leyendaAMARILLO.setPosition(leyendaAZUL.getPosition().x + leyendaAZUL.getLocalBounds().width + 20, window.getSize().y - 20);
+        sf::Text leyendaROJO("ROJO: Finger de un solo elemento", font);
+        leyendaROJO.setCharacterSize(24);
+        leyendaROJO.setFillColor(COLOR_ROJO);
 
-    window.draw(leyendaPLOMO);
-    window.draw(leyendaROJO);
-    window.draw(leyendaAZUL);
-    window.draw(leyendaAMARILLO);
+        leyendaROJO.setPosition(leyendaX - leyendaROJO.getLocalBounds().width, leyendaY + leyendaPLOMO.getLocalBounds().height);
 
-    window.display();
+        sf::Text leyendaAZUL("AZUL: Nodo sufijo o prefijo", font);
+        leyendaAZUL.setCharacterSize(24);
+        leyendaAZUL.setFillColor(COLOR_AZUL);
+
+        leyendaAZUL.setPosition(leyendaX - leyendaAZUL.getLocalBounds().width, leyendaY + leyendaPLOMO.getLocalBounds().height + leyendaROJO.getLocalBounds().height);
+
+        sf::Text leyendaAMARILLO("AMARILLO: Nodo", font);
+        leyendaAMARILLO.setCharacterSize(24);
+        leyendaAMARILLO.setFillColor(COLOR_AMARILLO);
+
+        leyendaAMARILLO.setPosition(leyendaX - leyendaAMARILLO.getLocalBounds().width, leyendaY + leyendaPLOMO.getLocalBounds().height + leyendaROJO.getLocalBounds().height + leyendaAZUL.getLocalBounds().height);
+
+        sf::Text leyendaNEGRO("NEGRO: Valor", font);
+        leyendaNEGRO.setCharacterSize(24);
+        leyendaNEGRO.setFillColor(COLOR_NEGRO);
+        leyendaNEGRO.setPosition(leyendaX - leyendaNEGRO.getLocalBounds().width, leyendaY + leyendaPLOMO.getLocalBounds().height + leyendaROJO.getLocalBounds().height + leyendaAZUL.getLocalBounds().height + leyendaAMARILLO.getLocalBounds().height);
+
+
+        window.draw(leyendaPLOMO);
+        window.draw(leyendaROJO);
+        window.draw(leyendaAZUL);
+        window.draw(leyendaAMARILLO);
+        window.draw(leyendaNEGRO);
+
+        window.display();
 
     while (window.pollEvent(event)) {
       if (event.type == sf::Event::Closed) {
@@ -97,11 +112,7 @@ int main() {
         if (in_push > 0) {
           // change status when enter is pressed. until then verify user input
           // to add only numbers
-          if ((event.text.unicode == 48 &&
-               ((value[0] == 45 && value.size() >= 2) ||
-                (value[0] != 45 && value.size() >= 1))) ||
-              (event.text.unicode >= 49 && event.text.unicode <= 57) ||
-              (event.text.unicode == 45 && value.empty())) {
+          if (event.text.unicode >= 48 && event.text.unicode <= 57) {
             value += (char)event.text.unicode;
           } else if (event.text.unicode == 13) {
             if (value == "") {
